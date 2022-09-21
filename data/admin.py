2238ca1a40ead9cd07dfe8033ce39a7fd8aea07c
alexismmdeveloper <alexismmdeveloper@gmail.com>
2022-09-20 18:31:57 -0700

    Create data models

--- data/admin.py
+++ data/admin.py
@@ -3,5 +3,4 @@ from data.models import Data
 
 @admin.register(Data)
 class DataAdmin(admin.ModelAdmin):
-    pass
-
+    list_display = ['username', 'created']
--- message/models.py
+++ message/models.py
@@ -4,7 +4,7 @@ from username.models import Username
 class Messages(models.Model):
     username = models.ForeignKey(Username, on_delete=models.CASCADE, related_name='messages')
     message = models.TextField(max_length=250)
-    createdDay = models.TimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.message
